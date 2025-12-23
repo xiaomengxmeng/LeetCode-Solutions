3e8a922c6a06fbe9e73b37c2bbdda8d4d4b9f052
xiaomengxmeng <z2914475697@163.com>
2025-12-23 20:15:33 +0800

    feat: 添加对称二叉树问题的递归解法
    实现递归方法判断二叉树是否对称，通过比较左右子树的镜像关系

---
+++ 101.symmetric-tree.cpp
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=cpp
+ * @lcpr version=30204
+ *
+ * [101] 对称二叉树
+ *
+ * https://leetcode.cn/problems/symmetric-tree/description/
+ *
+ * algorithms
+ * Easy (63.19%)
+ * Likes:    3049
+ * Dislikes: 0
+ * Total Accepted:    1.5M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[1,2,2,3,4,4,3]'
+ *
+ * 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：root = [1,2,2,3,4,4,3]
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：root = [1,2,2,null,3,null,3]
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [1, 1000] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool compail(TreeNode *p,TreeNode *q){
+        if(!p&&!q) return true;
+        if(!p||!q) return false;
+        return p->val==q->val&&compail(p->left,q->right)&&compail(p->right,q->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return compail(root->left,root->right);
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,3,4,4,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,2,null,3,null,3]\n
+// @lcpr case=end
+
+ */
+
