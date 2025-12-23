b798dc4d8df4158bb12c8a742afcbd64d71b2e9f
xiaomengxmeng <z2914475697@163.com>
2025-12-23 17:34:34 +0800

    feat: 实现67题二进制加法功能

--- 67.add-binary.cpp
+++ 67.add-binary.cpp
@@ -63,7 +63,20 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
+        int i=a.size()-1;
+        int j =b.size()-1;
+        int carry=0;
+        string res;
+        while(i>=0||j>=0|| carry) // 当两个字符串都遍历完，或者进位为0时，结束循环
+        {
+            int sum=carry;
+            if(j>=0) sum+=b[j--]-'0';
+            if(i>=0) sum+=a[i--]-'0';
+            carry=sum/2;
+            res+=to_string(sum%2);
+        }
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
 // @lc code=end
