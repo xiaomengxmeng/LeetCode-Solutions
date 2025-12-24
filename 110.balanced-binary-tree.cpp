d1cfe5e2782f12ac0346c14e219bb65d9dff667a
xiaomengxmeng <z2914475697@163.com>
2025-12-24 19:34:34 +0800

    feat: 添加平衡二叉树判断算法实现
    添加 LeetCode 110 题平衡二叉树的判断算法实现，使用递归方法计算子树高度并判断平衡性

---
+++ 110.balanced-binary-tree.cpp
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=cpp
+ * @lcpr version=30204
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode.cn/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.77%)
+ * Likes:    1660
+ * Dislikes: 0
+ * Total Accepted:    761.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是 平衡二叉树  
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：root = [1,2,2,3,3,null,null,4,4]
+ * 输出：false
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：root = []
+ * 输出：true
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中的节点数在范围 [0, 5000] 内
+ * -10^4 <= Node.val <= 10^4
+ *
+ *
+ */
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int getHeight(TreeNode *root)
+    {
+        if (root == nullptr)
+            return 0;
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1)
+            return -1;
+        else
+        {
+            return max(leftHeight, rightHeight) + 1;
+        }
+    }
+    bool isBalanced(TreeNode *root)
+    {
+        return getHeight(root) != -1;
+    }
+};
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [3,9,20,null,null,15,7]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,2,3,3,null,null,4,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
