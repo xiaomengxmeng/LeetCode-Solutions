8456eb76c3cdd129b23a9bb9cb0b6dba3d0b7cc6
xiaomengxmeng <z2914475697@163.com>
2025-12-25 20:46:54 +0800

    feat: 添加将有序数组转换为平衡二叉搜索树的解法
    实现递归构建平衡二叉搜索树的算法，通过找到数组中间元素作为根节点，然后递归构建左右子树

---
+++ 108.convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,115 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ * @lcpr version=30204
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ *
+ * https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (80.73%)
+ * Likes:    1739
+ * Dislikes: 0
+ * Total Accepted:    772.6K
+ * Total Submissions: 956.9K
+ * Testcase Example:  '[-10,-3,0,5,9]'
+ *
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums = [-10,-3,0,5,9]
+ * 输出：[0,-3,9,-10,null,5]
+ * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums = [1,3]
+ * 输出：[3,1]
+ * 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 按 严格递增 顺序排列
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    /*
+    递归构建平衡二叉搜索树
+    1. 找到数组的中间元素作为根节点
+    2. 递归构建左子树和右子树
+    3. 返回根节点
+    */
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        TreeNode* root = buildTree(nums, 0, nums.size() - 1);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& nums, int left, int right) {
+        if(left > right)
+            return nullptr;
+        int mid = left + (right - left) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = buildTree(nums, left, mid - 1);
+        root->right = buildTree(nums, mid + 1, right);
+        return root;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [-10,-3,0,5,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3]\n
+// @lcpr case=end
+
+ */
+
