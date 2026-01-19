dc69f3685c03451f4b7964839347fa8c11357b2e
xiaomengxmeng <z2914475697@163.com>
2026-01-19 20:23:11 +0800

    feat: 添加字符串相乘的算法实现

---
+++ 43.multiply-strings.cpp
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=43 lang=cpp
+ * @lcpr version=30204
+ *
+ * [43] 字符串相乘
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1=="0"||num2=="0") return "0";
+        int m = num1.size(), n = num2.size();
+        vector<int> res(m + n, 0);
+        for (int i = m - 1; i >= 0;i--)
+        {
+            for (int j = n - 1; j >= 0;j--)
+            {
+                int mul=(num1[i]-'0')*(num2[j]-'0');
+                res[i+j+1]+=mul;
+                res[i+j]+=res[i+j+1]/10;
+                res[i+j+1]%=10;
+            }
+        }
+        string ans="";
+        for(int i=0;i<res.size();i++)
+        {
+            if(i==0&&res[i]==0) continue;
+            ans+=to_string(res[i]);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "2"\n"3"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "123"\n"456"\n
+// @lcpr case=end
+
+ */
+
