00b72f22518c900ac3e30c4204d3dcde6e99e3cb
xiaomengxmeng <z2914475697@163.com>
2026-01-20 20:22:59 +0800

    feat: 添加两两交换链表节点的递归解法
    实现 LeetCode 24 题的递归解法，通过递归方式交换链表中的相邻节点

---
+++ 24.swap-nodes-in-pairs.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ * @lcpr version=30204
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /*
+        递归交换
+        递归交换链表中的节点，每次交换一对节点，返回新的头节点
+    */
+    ListNode* swapPairs(ListNode* head) {
+        if(head==nullptr||head->next==nullptr) return head;
+        ListNode* next = head->next;
+        head->next = swapPairs(next->next);
+        next->next = head;
+        return next;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
