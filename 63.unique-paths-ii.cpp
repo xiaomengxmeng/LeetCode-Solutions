314e0d41db2931dafe8e8c931aca934a55b2995f
xiaomengxmeng <z2914475697@163.com>
2026-01-22 20:37:18 +0800

    feat: 添加解决不同路径 II 问题的动态规划实现

---
+++ 63.unique-paths-ii.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ * @lcpr version=30204
+ *
+ * [63] 不同路径 II
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1 - obstacleGrid[0][0];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [[0,0,0],[0,1,0],[0,0,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,1],[0,0]]\n
+// @lcpr case=end
+
+ */
+
