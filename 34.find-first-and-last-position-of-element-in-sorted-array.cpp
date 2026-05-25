a0d0b7bbb482f2dce7542d25d8f08c5c1a444470
xiaomengxmeng <z2914475697@163.com>
2026-05-25 19:18:43 +0800

    chore: 完成704、35、34三道二分查找题的刷题记录
    新增对应题目的题解代码、每日学习记录与完成统计更新

---
+++ 34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=cpp
+ * @lcpr version=30204
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int leftbound = lower_bound(nums, target);
+        int rightbound = lower_bound(nums, target + 1);
+        if(leftbound == rightbound)
+            return {-1, -1};
+        return {leftbound, rightbound-1};
+    }
+    int lower_bound(vector<int> & nums,int target){
+        int left = 0, right = nums.size() - 1;
+        while(left<=right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid]>=target){
+                right = mid - 1;
+            }else if(nums[mid]<target)
+            {
+                left = mid + 1;
+            }else
+                return mid;
+        }
+        return left;
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,7,7,8,8,10]\n8\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,7,7,8,8,10]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n0\n
+// @lcpr case=end
+
+ */
+
