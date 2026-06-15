5178e614bd000749ed2bfe0a3be4fbb7aca6285d
xiaomengxmeng <z2914475697@163.com>
2026-06-15 16:12:02 +0800

    feat: 新增416分割等和子集题解与学习记录
    1. 新增leetcode 416题目的C++标准DP实现
    2. 添加当日学习笔记与题解复盘文档
    3. 更新做题统计与题目索引列表

---
+++ 416.partition-equal-subset-sum.cpp
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=cpp
+ * @lcpr version=30204
+ *
+ * [416] 分割等和子集
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        int n = nums.size();
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = true;
+            for (int j = 1; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+        }
+        return dp[n][target];
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,5,11,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,5]\n
+// @lcpr case=end
+
+ */
+
